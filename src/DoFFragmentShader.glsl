7e3ecb23a87f444ca7b80c3a7b2df191706c1e53
LorenzoAgnolucci <lory.agnolucci@gmail.com>
2020-07-23 18:24:43 +0200

    implement basic DoF fragment shader

--- src/DoFFragmentShader.glsl
+++ src/DoFFragmentShader.glsl
@@ -3,42 +3,39 @@
 varying vec2 vUv;
 uniform sampler2D tDiffuse;
 uniform sampler2D tDepth;
-uniform float cameraNear;
-uniform float cameraFar;
+uniform sampler2D tOriginal;
 
-uniform float focalDepth;       // Focal distance value in meters
-uniform float focalLength;      // Focal length in mm
-uniform float fstop;        // F-stop value
+uniform float bokehBlurSize;
+uniform float texelWidth;
+uniform float texelHeight;
 
-uniform float widthTexel;       // 1.0 / window.innerWidth
-uniform float heightTexel;      // 1.0 / window.innerHeight
-uniform bool horizontalBlur;        //True -> Horizontal, False -> Vertical
-
-
-float readDepth( sampler2D depthSampler, vec2 coord ) {
-    float fragCoordZ = texture2D( depthSampler, coord ).x;
-    float viewZ = perspectiveDepthToViewZ( fragCoordZ, cameraNear, cameraFar );
-    return viewZToOrthographicDepth( viewZ, cameraNear, cameraFar );
+float getWeight(float dist, float maxDist){
+    return 1.0 - dist/maxDist;
 }
 
 void main() {
-    float weight[7] = float[](0.1633, 0.1531, 0.12245, 0.0918, 0.051, 0.02, 0.009);
-    vec2 blurDirection;
-    if(horizontalBlur){
-        blurDirection = vec2(widthTexel, 0.0);
+    float bokehBlurWeightTotal = 0.0;
+    vec3 blurColor = vec3(0.0);
+
+    vec3 sourceColor = texture2D(tOriginal, vUv).rgb;
+
+    float CoC = max(texture2D(tDiffuse, vUv).g, texture2D(tDiffuse, vUv).b);
+
+    for (float i=-bokehBlurSize; i<bokehBlurSize+1.0; i++){
+        for (float j=-bokehBlurSize; j<bokehBlurSize+1.0; j++){
+            vec2 dir = vec2(i, j) * vec2(texelWidth, texelHeight);
+            float dist = length(dir);
+            if (dist > bokehBlurSize){
+                continue;
+            }
+            vec2 curUv = dir + vUv;
+            float weight = getWeight(dist, bokehBlurSize);
+            bokehBlurWeightTotal += weight;
+            blurColor +=  weight * texture2D(tOriginal, curUv).rgb;
+        }
     }
-    else{
-        blurDirection = vec2(0.0, heightTexel);
-    }
+    blurColor /= bokehBlurWeightTotal;
 
-    vec3 fragColor = texture2D(tDiffuse, vUv).rgb * weight[0];
-
-    for(int i=1; i<7; i++){
-        fragColor += texture2D(tDiffuse, (vUv + float(i) * blurDirection)).rgb * weight[i];
-        fragColor += texture2D(tDiffuse, (vUv - float(i) * blurDirection)).rgb * weight[i];
-    }
-
-    gl_FragColor.rgb = fragColor;
-
+    gl_FragColor.rgb = mix(sourceColor, blurColor, CoC);
     gl_FragColor.a = 1.0;
 }
